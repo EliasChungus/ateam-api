17e11a3f1d3e33243e53f71e3ed9e99dbfea5c75
elias <elias>
2023-12-14 08:10:08 +0100

    something

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,38 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as cfenv from 'cfenv';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as cfenv from "cfenv";
 
-import { AppEnv, GetAppEnvOptions } from 'cfenv';
+import { AppEnv, GetAppEnvOptions } from "cfenv";
 
 async function bootstrap() {
-  const appEnv: AppEnv = cfenv.getAppEnv();
-
-  console.log('Application name:', appEnv.name);
-  console.log('Port:', appEnv.port);
-  console.log('Binding host:', appEnv.bind);
-
-  const services = appEnv.getServices();
-  console.log('Services:', services);
-
   const customOptions: GetAppEnvOptions = {
-    name: 'customApp',
-    protocol: 'https',
+    name: "customApp",
+    protocol: "https",
     vcap: {
       application: {
-        name: 'CustomApplication',
+        name: "CustomApplication"
       },
       services: {
         customService: [
           {
             credentials: {
-              key: 'value',
-            },
-          },
-        ],
-      },
-    },
+              key: "value"
+            }
+          }
+        ]
+      }
+    }
   };
 
   const customAppEnv: AppEnv = cfenv.getAppEnv(customOptions);
-  console.log('Custom App name:', customAppEnv.name);
-  console.log('Custom Services:', customAppEnv.services);
 
-  const customAppEnv2: AppEnv = cfenv.getAppEnv();
-  console.log('Custom App name:', customAppEnv2.name);
-  console.log('Custom Services:', customAppEnv2.services);
+  const mariadbService = customAppEnv.getService('exact-service-name in cloud');
+  const mariadbURL = customAppEnv.getServiceURL('service-name');
+  const mariadbCreds = customAppEnv.getServiceCreds('service-name');
 
+  //Nest JS
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000); //replace
 }
+
 bootstrap();
